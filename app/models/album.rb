1aed173bc2bab00904f9f0f598c8ac71ee5580d7
Jaclyn Feminella <jaclynfeminella@Jaclyns-MacBook-Pro.local>
2016-05-07 22:00:49 -0400

    Add review controller, model, migration and associations

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,7 @@
 class Album < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+  has_many :reviews
 
   has_attached_file :album_image, styles: { album_index: "200x200>", album_show: "300x300>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :album_image, content_type: /\Aimage\/.*\Z/
